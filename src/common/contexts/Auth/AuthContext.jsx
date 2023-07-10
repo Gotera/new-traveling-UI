431f4fbb52dac836db6a2a4f4d503a83902e8360
OM <orlandomedeiros.junior@gmail.com>
2023-07-09 22:12:57 -0300

    Somes testes do Add a Auth to Login

---
+++ src/common/contexts/Auth/AuthContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+import { useApi } from "../../hooks/useApi";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const api = useApi();
+
+  const signin = async (email, password) => {
+    const data = await api.signin(email, password);
+    if (data.user && data.token) {
+      setUser(data.user);
+      return true;
+    }
+    return false;
+  };
+
+  const signout = async () => {
+    await api.logout();
+    setUser(null);
+  };
+  return (
+    <AuthContext.Provider value={{ signin, signout, user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Header from './components/Header';
-import History from './pages/History';
-import JorneyForm from './components/MainContent';
-import UserProvider from './common/contexts/User';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import History from "./pages/History";
+import JorneyForm from "./components/MainContent";
+import UserProvider from "./common/contexts/User";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function AppRouter() {
   return (
