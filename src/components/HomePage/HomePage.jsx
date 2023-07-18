d36f8b997ec943e956d329a0d70226cc3c348dc8
Gotera <guntokun5@gmail.com>
2023-07-18 16:39:08 -0300

    Content and ✨ Styles ✨ ADD to Home Page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import TruckImage1 from "../../assets/Home/TruckImage1.jpg";
-import { HomePageWrapper } from "./HomePage.styled";
+import BlueLogo from "../../assets/Home/BlueLogo.png";
+import {
+  AboutWrapper,
+  Apresentation,
+  HomePageWrapper,
+  SubTittle,
+} from "./HomePage.styled";
 
 function HomePage() {
   return (
     <>
       <HomePageWrapper>
-        <p>Home Page</p>
-        <img src={TruckImage1} />
+        <img src={TruckImage1} className="TruckBanner" />
+        <AboutWrapper>
+          <SubTittle>Sobre Nós</SubTittle>
+          <Apresentation>
+            <img src={BlueLogo} id="BlueLogo" />
+            <p>
+              {" "}
+              Nosso compromisso é registrar meticulosamente os dados de suas
+              viagens, cuidando de suas contas suas, proporcionando uma gestão
+              eficiente de rotas, lucros e gastos. Com a New Travels, você pode
+              focar na estrada, enquanto nós cuidamos do resto.
+            </p>
+          </Apresentation>
+        </AboutWrapper>
       </HomePageWrapper>
     </>
   );
--- src/components/HomePage/HomePage.styled.jsx
+++ src/components/HomePage/HomePage.styled.jsx
@@ -1,9 +1,48 @@
 import styled from "styled-components";
+import { colors } from "../../App.style";
 
 export const HomePageWrapper = styled.div`
-    img {
-        width: 100%;
-        height: 40rem;
-        object-fit: cover;
+  margin: 0 auto;
+  max-width: 1920px;
+  background-color: ${colors.babyBlue};
+  img {
+    &.TruckBanner {
+      width: 100%;
+      height: 40rem;
+      object-fit: cover;
+      bottom: 0;
+      object-position: center;
     }
-`
+  }
+`;
+
+export const AboutWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SubTittle = styled.h2`
+  font-size: 4rem;
+  margin: 2rem;
+  font-family: "Poppins", sans-serif;
+  color: ${colors.white};
+`;
+
+export const Apresentation = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  text-align: center;
+  align-items: center;
+
+  p {
+    font-family: "Poppins", sans-serif;
+    font-size: 1.5rem;
+    letter-spacing: 1.8px;
+    text-align: start;
+    color: ${colors.white};
+  }
+`;
