c73190c116edbfe9cac9cd3b593f6ab821e68ae1
Gotera <guntokun5@gmail.com>
2023-07-21 17:57:11 -0300

    Start of 📍 Footer Component 📍

---
+++ src/components/HomePage/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FooterWrapper } from "./Footer.style";
+
+function Footer() {
+    return <FooterWrapper>
+      
+  </FooterWrapper>;
+}
+
+export default Footer;
