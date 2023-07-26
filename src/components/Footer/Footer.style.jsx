d1db63352ca8268f6e6d16bd27f43fe4894f7041
Gotera <guntokun5@gmail.com>
2023-07-26 14:10:42 -0300

    Footer 🎀 Css 🎀 Implement

--- src/components/Footer/Footer.style.jsx
+++ src/components/Footer/Footer.style.jsx
@@ -4,29 +4,31 @@ import { colors } from "App.style";
 export const FooterWrapper = styled.div`
   background-color: ${colors.darkBlue};
   padding: 5% 5%;
-  img {
-    padding: 0 0% 5% 5%;
+  flex-wrap: wrap;
+`;
+
+export const ImgHorizontal = styled.div`
+  padding: 0 0% 5% 5%;
+  width: 100%;
+  @media (max-width: 600px) {
+    display: flex;
+    justify-content: center;
+    padding: 0 0% 5% 0;
+  }
+  @media (max-width: 300px) {
+    img {
+      display: none;
+    }
   }
 `;
+
 export const ColumnWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   padding: 0 5%;
-`;
-
-export const ColumLi = styled.li`
-  font-family: "Poppins", sans-serif;
-  color: ${colors.white};
-  font-weight: 500;
-  &:first-child {
-    margin-bottom: 1rem;
-    font-weight: 400;
-    color: ${colors.darkGrey};
+  @media (max-width: 600px) {
+    padding: 0;
+    flex-wrap: wrap;
   }
-`;
-export const ColumUl = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
 `;
